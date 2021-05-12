1ad5098148987cb7cc060d96b371edbb46f4c88e
lander-corral97 <lander.corral97@gmail.com>
2021-05-12 11:26:13 +0200

    Form y getMarcas (para select) hecho, aún sin funcionalidad

--- src/app/modules/principal/nuevo-coche/nuevo-coche.component.ts
+++ src/app/modules/principal/nuevo-coche/nuevo-coche.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ServiceService } from 'src/app/core/service/service.service';
+import { Marca } from 'src/app/shared/clase/marca';
 
 @Component({
   selector: 'app-nuevo-coche',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NuevoCocheComponent implements OnInit {
 
-  constructor() { }
+  marcas: Marca[] = [];
+  linkSelf: string[] = [];
+  id: number = 0;
+  nombre: string = '';
+
+  constructor(private service: ServiceService) { }
 
   ngOnInit(): void {
+    this.service.getMarcas().subscribe(
+      (datos: any) => {
+        datos._embedded.marcas.forEach((marcaAny:any) => {
+          this.linkSelf = marcaAny._links.self.href.split('/');
+          this.id = +this.linkSelf[this.linkSelf.length - 1];
+          this.nombre = marcaAny.nombre;
+
+          this.marcas.push(new Marca(this.id, this.nombre));
+        });
+      }
+    )
   }
 
 }
