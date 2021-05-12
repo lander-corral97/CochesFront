04ae5ac4616d9595df2b2dbdf559748759c00c50
lander-corral97 <lander.corral97@gmail.com>
2021-05-12 14:00:05 +0200

    Página terminada, con validación (quizá haya alguna forma más limpia...)

--- src/app/modules/principal/nuevo-coche/nuevo-coche.component.ts
+++ src/app/modules/principal/nuevo-coche/nuevo-coche.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ServiceService } from 'src/app/core/service/service.service';
 import { Marca } from 'src/app/shared/clase/marca';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Coche } from 'src/app/shared/clase/coche';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nuevo-coche',
@@ -13,10 +16,17 @@ export class NuevoCocheComponent implements OnInit {
   linkSelf: string[] = [];
   id: number = 0;
   nombre: string = '';
+  coche: Coche = new Coche('', '', 0);
 
-  constructor(private service: ServiceService) { }
+ 
+  nuevoCoche: FormGroup = new FormGroup({});
+  constructor(private service: ServiceService,
+    private router: Router,
+    private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    console.log(this.nuevoCoche);
+
     this.service.getMarcas().subscribe(
       (datos: any) => {
         datos._embedded.marcas.forEach((marcaAny:any) => {
@@ -27,7 +37,28 @@ export class NuevoCocheComponent implements OnInit {
           this.marcas.push(new Marca(this.id, this.nombre));
         });
       }
-    )
+    );
+    this.nuevoCoche = this.formBuilder.group({
+      marcas: [1],
+      modelo: ['', Validators.required],
+      matricula: ['', [Validators.required,Validators.pattern('[0-9]{1,4}( )[BCDFGHJKLMNPRSTVWXYZ]{2,3}')]]
+    });
+  }
+
+  get f() { return this.nuevoCoche.controls; }
+
+  nuevo() {
+    if (this.nuevoCoche.invalid) {
+      this.router.navigate(['/principal/nuevo']);
+    } else {
+      this.coche.marca = this.nuevoCoche.value.marcas;
+      this.coche.matricula = this.nuevoCoche.value.matricula;
+      this.coche.modelo = this.nuevoCoche.value.modelo;
+
+      this.service.postCoche({modelo: this.coche.modelo, matricula: this.coche.matricula, marca: "http://localhost:8080/marcas/" + this.coche.marca}). subscribe();
+
+      this.router.navigate(['/principal/lista']);
+    }
   }
 
 }
