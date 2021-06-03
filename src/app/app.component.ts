08443051f1d38abb410635adf7da209637632f05
lander-corral97 <lander.corral97@gmail.com>
2021-06-03 13:50:43 +0200

    La forma de verse dependerá de la ruta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CochesFront';
+  router: string = '';
+  parteRutas: string[] = [];
+
+  constructor(private ruta: Router) {
+    ruta.events.subscribe((val) => {
+      if (val instanceof NavigationEnd) {
+        this.router = val.url;
+      }
+    });
+  }
 }
